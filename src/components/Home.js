01601b9d7b1eec5a6a1add08260c2e05e43ba9c9
Andrew Cook <andrewjcook93@gmail.com>
2023-02-26 12:59:08 -0600

    add dummy animation for home to nav

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,28 +5,39 @@ import Navigation from './Navigation'
 
 function Home() {
   const [navClicked, setNavClicked] = useState(false);
-  const [navClass, setNavClass] = useState("navigation u-hidden");
+  const [navClass, setNavClass] = useState("u-hidden");
+
+  const openNav = () => {
+    if (!navClicked) {
+      setNavClass("")
+    } else {
+      setNavClass("u-hidden")
+    }
+  }
 
 
   return (
     <div className="home">
 
-      <div className="home__text-box">
-        <h1 className="heading-primary">
-          <span className="heading-primary--main">
-            Dwayne</span>
-          <span className="heading-primary--sub">
-            the Shell</span>
-      </h1>
-      </div>
+      <Navigation className={navClass}/>
+
+
+    <div className="home__text-box">
+      <h1 className="heading-primary">
+        <span className="heading-primary--main">
+          Dwayne</span>
+        <span className="heading-primary--sub">
+          the Shell</span>
+    </h1>
+    </div>
 
 
      <div className="home__img-box">
-      <img className="home__img" src="https://i.imgur.com/go7u9Zn.png" />
+      <img onClick={openNav} className="home__img" src="https://i.imgur.com/go7u9Zn.png" />
      </div>
 
 
-    <Navigation />
+    
 
 
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 function Navigation() {
+  const [navClicked, setNavClicked] = useState(false);
+  const [navClass, setNavClass] = useState("navigation u-hidden");
+
   return (
     <div className="navigation">
       <nav className="navigation__container">
+
         <Link className="navigation__link" data-text="HOME" to="/">Home</Link>
-        <Link className="navigation__link" data-text="JOURNALS" to="/">Journals</Link>
+
+        <Link className="navigation__link" data-text="JOURNAL" to="/journal">Journal</Link>
+
         <a className="navigation__link" data-text="INVENTORY" href="#">Inventory</a>
+
         <a className="navigation__link" data-text="ARTWORK" href="#">Artwork</a>
+
         <a className="navigation__link" data-text="HANDOUTS" href="#">Handouts</a>
       </nav>
     </div>
