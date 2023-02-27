b950dfd1b331cfe1225d9d7ae0ad4bd96330a8a1
Andrew Cook <andrewjcook93@gmail.com>
2023-02-27 15:37:48 -0600

    start horizontal scroll on journal page

--- src/components/Journal.js
+++ src/components/Journal.js
@@ -1,8 +1,61 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Journal() {
   return (
-    <div>Journal</div>
+    <div className="journal">
+
+      <h2 className="heading-secondary"><Link to="/navigate">Journal</Link></h2>
+
+      <div className="journal__main">
+
+        <div className="journal-card journal-card--1">
+          <div className="journal-card__text-box">
+            <span className="journal-card__text-box--main">
+              Introduction</span>
+            <span className="journal-card__text-box--sub">
+              11/06/2022</span>
+          </div>
+        </div> {/* 01 End */}
+
+        <div className="journal-card journal-card--2">
+          <div className="journal-card__text-box">
+            <span className="journal-card__text-box--main">
+              Strange Locket</span>
+            <span className="journal-card__text-box--sub">
+              12/03/2022</span>
+          </div>
+        </div> {/* 02 End */}
+
+        <div className="journal-card journal-card--3">
+          <div className="journal-card__text-box">
+            <span className="journal-card__text-box--main">
+            Boryet</span>
+            <span className="journal-card__text-box--sub">
+              02/25/2023</span>
+          </div>
+        </div> {/* 02 End */}
+        <div className="journal-card journal-card--2">
+          <div className="journal-card__text-box">
+            <span className="journal-card__text-box--main">
+            Boryet</span>
+            <span className="journal-card__text-box--sub">
+              02/25/2023</span>
+          </div>
+        </div> {/* 02 End */}
+        <div className="journal-card journal-card--2">
+          <div className="journal-card__text-box">
+            <span className="journal-card__text-box--main">
+            Boryet</span>
+            <span className="journal-card__text-box--sub">
+              02/25/2023</span>
+          </div>
+        </div> {/* 02 End */}
+
+      </div> 
+
+
+    </div>
   )
 }
 
