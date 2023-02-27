6396cfed7a3d936377a91f3ab7734ca786015489
Andrew Cook <andrewjcook93@gmail.com>
2023-02-27 17:10:28 -0600

    add more cards to journal

--- src/components/Journal.js
+++ src/components/Journal.js
@@ -66,6 +66,7 @@ function Journal() {
               02/25/2023</span>
           </div>
         </div> {/* 02 End */}
+
         <div className="journal-card journal-card--coming-soon">
           <div className="journal-card__text-box">
             <span className="journal-card__text-box--main">
@@ -73,7 +74,8 @@ function Journal() {
             <span className="journal-card__text-box--sub">
               02/25/2023</span>
           </div>
-        </div> {/* 02 End */}
+        </div> {/* CS End */}
+
         <div className="journal-card journal-card--coming-soon">
           <div className="journal-card__text-box">
             <span className="journal-card__text-box--main">
@@ -81,7 +83,25 @@ function Journal() {
             <span className="journal-card__text-box--sub">
               02/25/2023</span>
           </div>
-        </div> {/* 02 End */}
+        </div> {/* CS End */}
+
+        <div className="journal-card journal-card--coming-soon">
+          <div className="journal-card__text-box">
+            <span className="journal-card__text-box--main">
+            Coming Soon</span>
+            <span className="journal-card__text-box--sub">
+              02/25/2023</span>
+          </div>
+        </div> {/* CS End */}
+
+        <div className="journal-card journal-card--coming-soon">
+          <div className="journal-card__text-box">
+            <span className="journal-card__text-box--main">
+            Coming Soon</span>
+            <span className="journal-card__text-box--sub">
+              02/25/2023</span>
+          </div>
+        </div> {/* CS End */}
 
       </div> 
 
