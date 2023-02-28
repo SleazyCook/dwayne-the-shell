6cadab10d5c012b813ab023a871dc7a904be50eb
Andrew Cook <andrewjcook93@gmail.com>
2023-02-27 23:02:19 -0600

    add journals folder

---
+++ src/components/JournalEntries/01.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function 01() {
+  return (
+    <div>01</div>
+  )
+}
+
+export default 01
---
+++ src/components/JournalEntries/02.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function 02() {
+  return (
+    <div>02</div>
+  )
+}
+
+export default 02
